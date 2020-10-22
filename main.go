0e68538deaae8c536dccece6b830d5afc0650d32
NachoGranados <ignaciogramar@gmail.com>
2020-10-21 21:59:46 -0600

    New api code

--- main.go
+++ main.go
@@ -4,20 +4,41 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"pokemon-api/database"
 )
 
 func getAllPokemons(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode(database.PokemonDb)
+	json.NewEncoder(w).Encode(database.PokemonDbAsValueArray())
+}
+
+func addPokemon(w http.ResponseWriter, r *http.Request) {
+	var newPokemon database.Pokemon
+
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	json.Unmarshal(reqBody, &newPokemon)
+
+	if _, ok := database.PokemonDb[newPokemon.ID]; ok {
+		w.WriteHeader(http.StatusNotModified)
+		return
+	}
+	database.PokemonDb[newPokemon.ID] = newPokemon
+	w.WriteHeader(http.StatusOK)
 }
 
 func handleRequests() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
-	log.Fatal(http.ListenAndServe(":10000", myRouter))
+	myRouter.HandleFunc("/pokemons", addPokemon).Methods("POST")
+	log.Fatal(http.ListenAndServe(":"+port, myRouter))
 }
 
 func commonMiddleware(next http.Handler) http.Handler {
@@ -31,6 +52,4 @@ func main() {
 	fmt.Println("Pokemon Rest API")
 	handleRequests()
 }
-
-// Hello world
 
