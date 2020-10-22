29e74bf2b630e69e729affcf033836a0269f57d6
NachoGranados <ignaciogramar@gmail.com>
2020-10-22 01:12:05 -0600

    Deleting comments

--- main.go
+++ main.go
@@ -10,51 +10,63 @@ import (
 	"os"
 	"pokemon-api/database"
 )
-//holaaaaa
-func getAllPokemons(w http.ResponseWriter, r *http.Request) {
+
+func getAllPokemons(w http.ResponseWriter, r *http.Request)  {
+
 	json.NewEncoder(w).Encode(database.PokemonDbAsValueArray())
+
 }
-//holaaaaa
+
 func addPokemon(w http.ResponseWriter, r *http.Request) {
+
 	var newPokemon database.Pokemon
-	//holaaaaa
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(reqBody, &newPokemon)
-	//holaaaaa
+
 	if _, ok := database.PokemonDb[newPokemon.ID]; ok {
+
 		w.WriteHeader(http.StatusNotModified)
 		return
+
 	}
+
 	database.PokemonDb[newPokemon.ID] = newPokemon
 	w.WriteHeader(http.StatusOK)
+
 }
-//holaaaaa
+
 func handleRequests() {
+
 	port := os.Getenv("PORT")
+
 	if port == "" {
+
 		port = "80"
+
 	}
+
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
 	myRouter.HandleFunc("/pokemons", addPokemon).Methods("POST")
 	log.Fatal(http.ListenAndServe(":"+port, myRouter))
+
 }
-//holaaaaa
+
 func commonMiddleware(next http.Handler) http.Handler {
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
+
 	})
+
 }
-//holaaaaa
+
 func main() {
+
 	fmt.Println("Pokemon Rest API")
 	handleRequests()
+
 }
-//holaaaaa
-//holaaaaa
-//holaaaaa
-//holaaaaa
-//holaaaaa
-//holaaaaa
