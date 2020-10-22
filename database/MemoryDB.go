349bab980192d97c27e158a4ed2c9750b370c82c
NachoGranados <ignaciogramar@gmail.com>
2020-10-22 01:18:53 -0600

    Reorganizing the project files

--- database/MemoryDB.go
+++ database/MemoryDB.go
@@ -1,32 +1,22 @@
 package database
 
 type Pokemon struct {
-
 	ID   string `json:"Id"`
 	Name string `json:"Name"`
 	Type string `json:"Type"`
-
 }
 
 var PokemonDb = map[string]Pokemon{
-
 	"1": Pokemon{ID: "1", Name: "Pikachu", Type: "Electric"},
 	"2": Pokemon{ID: "2", Name: "Charmeleon", Type: "Fire"},
-
 }
 
 func PokemonDbAsValueArray() []Pokemon {
-
 	valueArray := make([]Pokemon, len(PokemonDb))
 	i := 0
-
 	for _, value := range PokemonDb {
-
 		valueArray[i] = value
 		i++
-
 	}
-
 	return valueArray
-
 }
--- main.go
+++ main.go
@@ -11,62 +11,44 @@ import (
 	"pokemon-api/database"
 )
 
-func getAllPokemons(w http.ResponseWriter, r *http.Request)  {
-
+func getAllPokemons(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(database.PokemonDbAsValueArray())
-
 }
 
 func addPokemon(w http.ResponseWriter, r *http.Request) {
-
 	var newPokemon database.Pokemon
+
 	reqBody, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(reqBody, &newPokemon)
 
 	if _, ok := database.PokemonDb[newPokemon.ID]; ok {
-
 		w.WriteHeader(http.StatusNotModified)
 		return
-
 	}
-
 	database.PokemonDb[newPokemon.ID] = newPokemon
 	w.WriteHeader(http.StatusOK)
-
 }
 
 func handleRequests() {
-
 	port := os.Getenv("PORT")
-
 	if port == "" {
-
 		port = "80"
-
 	}
-
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.Use(commonMiddleware)
 	myRouter.HandleFunc("/pokemons", getAllPokemons).Methods("GET")
 	myRouter.HandleFunc("/pokemons", addPokemon).Methods("POST")
 	log.Fatal(http.ListenAndServe(":"+port, myRouter))
-
 }
 
 func commonMiddleware(next http.Handler) http.Handler {
-
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-
 		w.Header().Add("Content-Type", "application/json")
 		next.ServeHTTP(w, r)
-
 	})
-
 }
 
 func main() {
-
 	fmt.Println("Pokemon Rest API")
 	handleRequests()
-
 }
